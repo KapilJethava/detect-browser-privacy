9183cf558f6a73302540e009d912b7d27db9b411
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-04-18 11:36:20 +0530

    internal error resolved

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var app = new express();
+var express = require('express'),
+	path = require('path'),
+	app = new express();
+
 
 app.use('/', express.static(__dirname));
 // app.use('/node_modules', express.static(__dirname) + "/node_modules");
@@ -7,8 +9,9 @@ app.use('/', express.static(__dirname));
 // app.use('/controller', express.static(__dirname) + "/controller");
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '/index.html'));
+	res.sendFile(path.join(__dirname , '/index.html'));
 });
 app.listen(process.env.PORT || 4200, function(){
+	console.log(__dirname);
 	console.log("server listening on port 4200, please browse to http://localhost:4200")
 })
