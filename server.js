76cd7dc4344fb883a8985348d1f852833cbd9d35
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-04-18 13:17:19 +0530

    test implementation

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-var express = require('express'),
-	app = new express();
+// var express = require('express'),
+// 	app = new express();
 
-app.use(express.static(__dirname));
-app.get('/',function(req,res){
-	res.render('index.html')
-})
+// app.use(express.static(__dirname));
+// app.get('/',function(req,res){
+// 	res.render('index.html')
+// })
 
-app.listen(process.env.PORT || 4200, function () {
-	console.log(__dirname);
-	console.log("server listening on port 4200, please browse to http://localhost:4200")
-})
+// app.listen(process.env.PORT || 4200, function () {
+// 	console.log(__dirname);
+// 	console.log("server listening on port 4200, please browse to http://localhost:4200")
+// })
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+	.use(express.static(__dirname))
+	.get('/', (req, res) => res.render('index'))
+	.listen(PORT, () => console.log(`Listening on ${PORT}`))
