387415480581b10b7d7c57688fa6c69259967b8b
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-04-18 12:07:43 +0530

    rendered the page

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use('/', express.static(__dirname));
 // app.use('/controller', express.static(__dirname) + "/controller");
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname , '/index.html'));
+	res.render('index.html');
 });
 app.listen(process.env.PORT || 4200, function(){
 	console.log(__dirname);
