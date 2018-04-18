8b0071ba80e24c90725a4f44419f4f785b8255e7
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-04-18 11:18:31 +0530

    code improved

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = new express();
-app.use('/', express.static('public'))
 app.use(express.static(__dirname));
-app.listen(4200, function(){
+app.listen(process.env.PORT || 4200, function(){
 	console.log("server listening on port 4200, please browse to http://localhost:4200")
 })
