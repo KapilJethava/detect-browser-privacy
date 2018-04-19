1b2068b99137db79467dd5240760c17db0f81110
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-04-19 15:15:17 +0530

    dsafasd

--- server.js
+++ server.js
@@ -17,19 +17,18 @@ const PORT = process.env.PORT || 5000
 
 express()
 	.use(express.static(__dirname))
-
-	// .use(express.static(__dirname + '/assets/css'))
-	// .use(express.static(__dirname + '/node_modules'))
-	// .use(express.static(__dirname + '/controller'))
-	// .use(express.static(__dirname + '/service'))
-	// .get('/', (req, res) => {
-	// 	try{
-	// 		res.set('Content-Type', 'text/html');
-	// 		res.render('index.html');
-	// 	} catch(e){
-	// 		res.status(200).send(e);
-	// 	}
-	// })
+	.use(express.static(__dirname + '/assets/css'))
+	.use(express.static(__dirname + '/node_modules'))
+	.use(express.static(__dirname + '/controller'))
+	.use(express.static(__dirname + '/service'))
+	.get('/', (req, res) => {
+		try{
+			res.set('Content-Type', 'text/html');
+			res.render('index.html');
+		} catch(e){
+			res.status(200).send(e);
+		}
+	})
 	.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 // const http = require('http');
