bba8038dbd703ce910e1b96048fe02a3f527d0b4
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-04-19 12:13:37 +0530

    new attempt

--- server.js
+++ server.js
@@ -13,9 +13,13 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+console.log(__dirname + '/service');
 express()
 	.use(express.static(__dirname))
+	.use(express.static(__dirname + '/assets/css'))
+	.use(express.static(__dirname + '/node_modules'))
+	.use(express.static(__dirname + '/controller'))
+	.use(express.static(__dirname + '/service'))
 	.get('/', (req, res) => {
 		try{
 			res.sendFile('index.html');
@@ -24,3 +28,19 @@ express()
 		}
 	})
 	.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+// const http = require('http');
+// const fs = require('fs');
+
+// const onRequest = (req, res) => {
+// 	res.writeHead(200, {'Content-Type':'text/html'});
+// 	fs.readFile('./index.html', (err, data)=>{
+// 		if(err){
+// 			res.write('you will resolve this in next attempt');
+// 		} else {
+// 			res.write(data);
+// 		}
+// 		res.end();
+// 	})
+// }
+// http.createServer(onRequest).listen(process.env.PORT || 6666);
