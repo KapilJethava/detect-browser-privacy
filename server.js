66ddeec5366e021c1c16a06d50e2d724edd6883a
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-04-19 12:57:03 +0530

    adsfas

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ express()
 	// .use(express.static(__dirname + '/service'))
 	.get('/', (req, res) => {
 		try{
+			res.set('Content-Type', 'text/html');
 			res.sendFile('Index.html');
 		} catch(e){
 			res.status(200).send(e);
