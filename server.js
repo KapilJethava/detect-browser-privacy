5ecbecb4bcd97b1972222484251476457027e4cd
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-04-19 11:16:15 +0530

    finally good in local

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ const PORT = process.env.PORT || 5000
 
 express()
 	.use(express.static(__dirname))
-	.get('/', (req, res) => res.render('index'))
+	.get('/', (req, res) => res.render('index.html'))
 	.listen(PORT, () => console.log(`Listening on ${PORT}`))
