81929b5c435f2cbf366273bf7762d156ca5bca4a
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-04-19 12:27:17 +0530

    adfasd

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000
 express()
 	.use(express.static(__dirname))
 	.get('/', (req, res)=>{
-		res.send('Kapil')
+		res.send('index.html')
 	})
 	// .use(express.static(__dirname + '/assets/css'))
 	// .use(express.static(__dirname + '/node_modules'))
